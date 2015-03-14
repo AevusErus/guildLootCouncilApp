a0fe09b98a2f7710c0fb4a10040e086927aebb9b
AevusErus <ssugs.scififan@yahoo.com>
2015-03-14 15:37:18 -0600

    Continuing to get the project set up with dependencies and libraries.

---
+++ config.rb
@@ -0,0 +1,5 @@
+css_dir = "public/stylesheets"
+sass_dir = "public/sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Omega Res Novae Loot Council Assistant' });
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 module.exports = router;
