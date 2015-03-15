57564f9a9425e10afdb0c63e8e099726f03b62aa
AevusErus <ssugs.scififan@yahoo.com>
2015-03-14 22:44:28 -0600

    Getting html view set up.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Omega Res Novae Loot Council Assistant' });
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.render('index', { title: 'Omega Res Novae Loot Council Assistant' });
+  // res.sendFile(path.join(__dirname + '/plain.html'));
 });
 
 module.exports = router;
